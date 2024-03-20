c0d4c64d30b0368ade43eb21797e6c7e40e34125
emwu1024 <emwu1024@gmail.com>
2024-03-20 18:30:29 +0000

    wip: homepage

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
-import { useMediaQuery } from 'react-responsive';
-import { NavLink } from 'react-router-dom';
-import { IoClose, IoMenu } from 'react-icons/io5';
+import React, { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import { NavLink } from "react-router-dom";
+import { IoClose, IoMenu } from "react-icons/io5";
 
-import './Navbar.css';
-import logo from '../../assets/resippy-logo-v2.png';
+import "./Navbar.css";
+// import logo from '../../assets/resippy-logo-v2.png';
+import logo from "../../assets/stamp-logo-red-round-2.png";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const isMobile = useMediaQuery({ maxWidth: '1100px' });
+  const isMobile = useMediaQuery({ maxWidth: "1100px" });
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -23,8 +24,8 @@ const Navbar = () => {
   //   Conditionally rendered navbar code
   // Real talk this will need some work as i've implemented the list way differently to them oop
   const renderNavLinks = () => {
-    const listClassName = isMobile ? 'nav-list-mobile' : 'nav-list';
-    const listContainerClassName = isMobile ? '' : 'list-container';
+    const listClassName = isMobile ? "nav-list-mobile" : "nav-list";
+    const listContainerClassName = isMobile ? "" : "list-container";
 
     return (
       <div className={listContainerClassName}>
@@ -69,17 +70,11 @@ const Navbar = () => {
     <div>
       <header className="header-container">
         <nav
-          className={`nav-container  ${isMobile ? 'nav-container-mobile' : ''}`}
+          className={`nav-container  ${isMobile ? "nav-container-mobile" : ""}`}
         >
-          <div className="bar-hamburger">
-            <p>hello</p>
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </div>
           {isMobile && (
             <div
-              className={`hamburger  ${isMenuOpen ? 'hide-icon' : 'show-icon'}`}
+              className={`hamburger  ${isMenuOpen ? "hide-icon" : "show-icon"}`}
               id="nav-toggle"
               onClick={toggleMenu}
             >
@@ -89,13 +84,13 @@ const Navbar = () => {
 
           {isMobile ? (
             <div
-              className={`${isMenuOpen ? 'show-menu' : 'hide-menu'}`}
+              className={`${isMenuOpen ? "show-menu" : "hide-menu"}`}
               id="nav-menu"
             >
               {renderNavLinks()}
               <div
                 className={`nav-close  ${
-                  isMenuOpen ? 'show-icon' : 'hide-icon'
+                  isMenuOpen ? "show-icon" : "hide-icon"
                 }`}
                 id="nav-close"
                 onClick={toggleMenu}
