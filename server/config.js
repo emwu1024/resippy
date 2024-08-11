1baed2b7edc59d5a7d19988990c777614e48229b
Emily <49752149+emwu1024@users.noreply.github.com>
2024-08-11 12:40:11 +0100

    Cleanup

--- server/config.js
+++
@@ -1,5 +0,0 @@
-// WHEN DEBUGGING CHECK UR USING PORT 8000 NOT 5555 IN THE URL
-export const PORT = 8000; // ES6 module syntax
-
-export const mongoDBURL =
-  'mongodb+srv://cass:everythingbagel0@resippy-cluster.0qp0892.mongodb.net/resippy_db?retryWrites=true&w=majority';
